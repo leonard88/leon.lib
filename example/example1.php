fce0fcf3ff8be0f8adc16ed7da29aa88c622f69c
Leonard Green <me@leon88.com>
2012-05-04 13:06:25 +0200

    Example to the usage of init.

--- example/example1.php
+++ example/example1.php
@@ -7,5 +7,25 @@
  * File to show examples about using leon.lib.
  */
 
-    echo 'Hello World!';
+    //How to use leon.lib
+
+    //Initializing
+
+    require_once("../Init.php"); //you can use an auto-loader too.
+    
+    echo "<pre>";
+    
+    //Prepares the leon.lib.
+    //The true parameter indicates the we want to verbose msgs.
+    leon\lib\Init::prepare(true);
+    //Test: Is leon.lib initialized?
+    leon\lib\Init::test();
+    
+    //Cant be used after destroy. 
+    leon\lib\Init::destroy();
+    leon\lib\Init::test();
+
+    echo "</pre>";
+    
+    //Example 2 shows an auto-loader solution.
 ?>
