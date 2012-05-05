40fb105138faf0954c4a49f39e3a51bbd3ea9e6e
Leonard Green <me@leon88.com>
2012-05-05 17:59:29 +0200

    Base::call() function for call a bunch of same objs with a given method and an optional parameter

--- lib/Base.php
+++ lib/Base.php
@@ -36,6 +36,24 @@ class Base {
         return $array;
     }
     
+    /**
+     * This will call a method of one or a bunch of $obj(s).
+     * If $obj is array it will call all of the members of the array with the given 
+     * method.
+     * $param is optional. Call the $method with a $param.
+     * @param mixed $obj Could be an array (wich is containg objs) or one object.
+     * @param string $method
+     * @param mixed $param 
+     */
+    static function call($obj, $method, $param = null){
+        
+        if (is_array($obj)) foreach($obj as $value){
+            if ($param) $value->$method($param);
+            else $value->$method();
+        }
+        else if ($param) $obj->$method($param);
+        else $obj->$method();
+    }
 }
 
 ?>
